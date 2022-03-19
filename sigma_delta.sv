38cd8d67ca981a5930eddef701022e97f0e87097
Tom Kuzma <t_kuzma@hotmail.com>
2022-03-18 18:32:17 -0700

    update sigma delta

--- sigma_delta.sv
+++ sigma_delta.sv
@@ -5,11 +5,16 @@ module sigma_delta  #(parameter N = 10) (
 );
 
 logic [N:0] PWM_accumulator;
+logic [N-1:0] accumulator_next;
 
 always_ff @(posedge clk, negedge reset_n) begin
-    PWM_accumulator <= PWM_accumulator[N-1:0] + ADC_in;
+    PWM_accumulator <= accumulator_next + ADC_in;
 
 end 
+
+always_comb begin
+    accumulator_next = PWM_accumulator[N-1:0];
+end
 
 assign pwm_out = PWM_accumulator[8];
 
