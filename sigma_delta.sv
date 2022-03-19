0292e6d1ccf0f382070dece320f2dce9ce5887cb
Tom Kuzma <t_kuzma@hotmail.com>
2022-03-18 18:37:22 -0700

    fixed sigma output

--- sigma_delta.sv
+++ sigma_delta.sv
@@ -8,14 +8,10 @@ logic [N:0] PWM_accumulator;
 logic [N-1:0] accumulator_next;
 
 always_ff @(posedge clk, negedge reset_n) begin
-    PWM_accumulator <= accumulator_next + ADC_in;
+    PWM_accumulator <= PWM_accumulator[N-1:0] + ADC_in;
 
 end 
 
-always_comb begin
-    accumulator_next = PWM_accumulator[N-1:0];
-end
-
-assign pwm_out = PWM_accumulator[8];
+assign pwm_out = PWM_accumulator[N];
 
 endmodule
